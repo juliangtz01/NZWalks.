9cf94caf7c426e9fbd9c13d7c17a0009edc11d70
Julian Gutierrez <julian_m_gtz@yahoo.com>
2023-02-10 23:59:47 -0600

    Added Navigation properties

--- NZWalks.API/Models/Domain/Walk.cs
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -8,5 +8,11 @@
         public double Length { get; set; }
         public Guid RegionId { get; set; }
         public Guid WalkDifficultyId { get; set; }
+
+        // Navigation Properties
+
+        public Region Region { get; set; }
+        public WalkDifficulty WalkDifficulty { get; set; }
+
     }
 }
