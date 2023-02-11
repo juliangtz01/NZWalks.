45153cfaf8ccde9c4c703abd36e610e0f830a389
Julian Gutierrez <julian_m_gtz@yahoo.com>
2023-02-10 23:34:11 -0600

    Created Region and Walk calsses along with Models and Domain folders

---
+++ NZWalks.API/Models/Domain/Walk.cs
@@ -0,0 +1,12 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Walk
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public Guid RegionId { get; set; }
+        public Guid WalkDifficultyId { get; set; }
+    }
+}
