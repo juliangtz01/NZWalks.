8217a5f2fe9c5723c00fb638f16646c5264c4783
Julian Gutierrez <julian_m_gtz@yahoo.com>
2023-02-14 12:12:19 -0600

    Created new repository method for GetAllWalks

--- NZWalks.API/Repositories/IWalkRepository.cs
+++ NZWalks.API/Repositories/IWalkRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace NZWalks.API.Repositories
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
 {
     public interface IWalkRepository
     {
+        Task<IEnumerable<Walk>> GetAllAsync();
     }
 }
--- NZWalks.API/Repositories/WalkRepository.cs
+++ NZWalks.API/Repositories/WalkRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace NZWalks.API.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
 {
-    public class WalkRepository: IWalkRepository
+    public class WalkRepository : IWalkRepository
     {
+        private readonly NZWalksDbContext nZWalksDbContext;
+
+        public WalkRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+
+        public async Task<IEnumerable<Walk>> GetAllAsync()
+        {
+            return await nZWalksDbContext.Walks.ToListAsync();
+        }
     }
 }
