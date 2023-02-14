ac20f82f531bd72f00d70708e6be34ba72f13801
Julian Gutierrez <julian_m_gtz@yahoo.com>
2023-02-14 12:33:21 -0600

    Created a new controller and method for GetAllWalks

---
+++ NZWalks.API/Controllers/WalksController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("Controller")]
+    public class WalksController : Controller
+    {
+        private readonly IWalkRepository walkRepository;
+        private readonly IMapper mapper;
+
+        public WalksController(IWalkRepository walkRepository, IMapper mapper)
+        {
+            this.walkRepository = walkRepository;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> GetAllWalksAsync()
+        {
+            // Fetch data from database - domain walks
+            var walksDomain = await walkRepository.GetAllAsync();
+
+            // Convert domain walks to DTO Walks
+            var walksDTO = mapper.Map<List<Models.DTO.Walk>>(walksDomain);
+
+            //Return response
+            return Ok(walksDTO);
+        }
+    }
+}
